2e978907bddb1ebf3331aba197ca445ab57375bd
mikael0929 <44366127+mikael0929@users.noreply.github.com>
2025-05-30 18:34:43 +0900

    서버오픈 준비
    const socket = io("http://localhost:3001");
    
    를
    
    const socket = io("https://coop-maze-server.onrender.com"); 로 교체

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
-//const socket = io("https://coop-maze-server.onrender.com");
+const socket = io("https://coop-maze-server.onrender.com");
 
-const socket = io("http://localhost:3001");
+//const socket = io("http://localhost:3001");
 
 const allRoles = ["a", "b", "c", "d", "e"];
 
